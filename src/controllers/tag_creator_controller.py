59845ed2a263ba01a0935aa850538e84576b066e
junioralcant <junioralcant111@gmail.com>
2024-02-06 15:37:40 -0300

    feat: create TagCreateController

---
+++ src/controllers/tag_creator_controller.py
@@ -0,0 +1,24 @@
+
+from typing import Dict
+from src.drivers.barcode_handler import BarcodeHandler
+
+
+class TagCreateController:
+    def create(self, product_code: str) -> Dict:
+        path_from_tag = self.__create_tag(product_code)
+        return self.__format_response(path_from_tag)
+
+    def __create_tag(self, product_code: str) -> str:
+        barcode_handle = BarcodeHandler()
+        path_from_tag = barcode_handle.create_barcode(product_code)
+        return path_from_tag
+
+    def __format_response(self, path_from_tag: str) -> Dict:
+        return {
+            "data": {
+                "type": "Tag Image",
+                "count": 1,
+                "path": f'{path_from_tag}.png'
+            }
+        }
+    
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,10 +1,16 @@
+from src.controllers.tag_creator_controller import TagCreateController
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 
 
 class TagCreatorView:
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreateController()
+
         body = http_request.body
         product_code = body["product_code"]
-        return HttpResponse(status_code=200, body={"product_code": product_code})
+
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
     
